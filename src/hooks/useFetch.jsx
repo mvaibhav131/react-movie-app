4761ccd41c147afebf19179979863fda42e68d32
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-04 01:26:49 +0530

    :dvd: create data fetch comp

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { fetchData } from "../utils/api";
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setLoading("loading...");
+        setData(null);
+        setError(null);
+
+        fetchData(url)
+            .then((res) => {
+                setLoading(false);
+                setData(res);
+            })
+            .catch((err) => {
+                console.log(err)
+                setLoading(false);
+                setError("Something went wrong!");
+            });
+    }, [url]);
+
+    return { data, loading, error };
+};
+
+export default useFetch;

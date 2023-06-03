113fb1ee6ae6f86becb58471790ba95d5d6cf8b7
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-04 01:23:52 +0530

    :+1: configuration url set

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,51 @@
 
+import { useEffect } from 'react';
 import './App.scss'
+import { fetchData } from './utils/api';
+import { useDispatch, useSelector } from 'react-redux';
+import { getApiConfiguration } from './store/homeSlice';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import SearchResult from './pages/searchResult/SearchResult';
+import Explore from './pages/explore/Explore';
+import {PageNotFound} from './pages/404/PageNotFound';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
+  const dispatch=useDispatch();
+  const {url}=useSelector((state)=>state.home)
+  console.log(url)
+useEffect(()=>{
+fetchApiConfig()
+},[])
+
+  const fetchApiConfig=()=>{
+    fetchData("/configuration")
+    .then((res)=>{
+      console.log(res);
+      const url={
+        backdrop: res.images.secure_base_url+"original",
+        poster: res.images.secure_base_url+"original",
+        profile: res.images.secure_base_url+"original"
+      }
+      dispatch(getApiConfiguration(url));
+    });
+ };
 
   return (
-    <>
-     <div>Hello</div>
-    </>
+  <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path="/" element={<Home/>}  />
+    <Route path="/:mediaType/:id" element={<Details/>}  />
+    <Route path="/search/:query" element={<SearchResult/>}  />
+    <Route path="/explore/:mediaType" element={<Explore/>}  />
+    <Route path="*" element={<PageNotFound/>}  />
+  </Routes>
+  <Footer/>
+  </BrowserRouter>
   )
 }
 

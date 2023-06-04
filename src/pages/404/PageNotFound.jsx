d2e330870fa1a2b629d4ad544e0906c8b2ca0103
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-05 01:07:01 +0530

    :feet: 404 function added with scss

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,12 +1,19 @@
+import React from "react";
 
-import React from 'react'
+import "./style.scss";
 
- export const PageNotFound = () => {
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+export const PageNotFound = () => {
   return (
-    <div>
-      pnf
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </ContentWrapper>
     </div>
-  )
-}
+  );
+};
+
 
 

774f91f24a25ef2490b1750b0117d0792d40d04d
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-05 01:05:03 +0530

    :fu: genres function , scss done

---
+++ src/components/genres/Genres.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import "./style.scss";
+
+const Genres = ({ data }) => {
+    const{genres}=useSelector((state)=> state.home)
+  return (
+      <div className="genres">
+          {data?.map((i) => {
+              if (!genres[i]?.name) return;
+              return (
+                  <div key={i} className="genre">
+                      {genres[i]?.name}
+                  </div>
+              )
+          })}
+    </div>
+  )
+}
+
+export default Genres

ff9724971984ae0f99a10dd59720593cba6eed28
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-05 01:11:30 +0530

    :+1: home section completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import "./style.scss";
 import HeroBanner from './heroBanner/HeroBanner';
 import Trending from './trending/Trending';
+import Popular from './popular/Popular';
+import TopRated from './topRated/TopRated';
+import Upcoming from './upcoming/Upcoming';
 
 const Home = () => {
   return (
     <div className='homePage'>
       <HeroBanner />
-      <Trending/>
+      <Upcoming />
+      <Trending />
+      <Popular />
+      <TopRated />
     </div>
   )
 }

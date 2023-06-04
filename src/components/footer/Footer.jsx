2729d813640856416a8620e853ea1108af00a93f
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-04 14:14:44 +0530

    :+1: footerfunction done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,50 @@
-// import React from 'react'
+import React from "react";
+import {
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+  FaGithub,
+  FaGitlab,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
 import "./style.scss";
 
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
-  )
-}
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <a style={{ color: "white",textDecoration:"none" }} href="https://vaibhav-more-portfolio-site.netlify.app/"><li  className="menuItem">About</li></a>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Made By Vaibhav More (Full stack Developer).
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <a style={{ color: "white" }} href="https://github.com/mvaibhav131"><FaGithub /></a>
+          </span>
+          <span className="icon">
+            <a style={{ color: "white" }} href="https://vaibhav-more-portfolio-site.netlify.app/"><FaGitlab /></a> 
+          </span>
+          <span className="icon">
+            <a style={{ color: "white" }} href="https://www.instagram.com/vaibhavmore1997/"><FaInstagram /></a> 
+          </span>
+          <span className="icon">
+            <a style={{ color: "white" }} href="https://twitter.com/mvaibhav131"><FaTwitter /></a> 
+          </span>
+          <span className="icon">
+            <a style={{ color: "white" }} href="https://www.linkedin.com/in/vaibhav131/"><FaLinkedin /></a> 
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
 
 export default Footer;

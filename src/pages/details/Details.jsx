4e3c3770611606cd2b70388320a9be007dd35a13
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-05 01:08:38 +0530

    :+1: Main detail component

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import "./style.scss"
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
+import DetailsBanner from '../detailsBanner/DetailsBanner';
+import Cast from './cast/Cast';
+import VideosSection from './videosSection/VideosSection';
+import Similar from './carousels/Similar';
+import Recommendation from './carousels/Recommendation';
 
 const Details = () => {
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data:credits, loading:creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
+
+
   return (
     <div>
-      Detail
-    </div>
-  )
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id}/>
+  </div>
+    )
 }
 
 export default Details;

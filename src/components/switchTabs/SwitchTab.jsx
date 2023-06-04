2d0d5676a0fcb7eaea97b13e26ef831322ab1e7d
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-05 01:06:03 +0530

    :crown: switch tab done

---
+++ src/components/switchTabs/SwitchTab.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import "./style.scss";
+
+const SwitchTab = ({ data, onTabChange }) => {
+    const [selectedTab, setSelectedTab] = useState(0);
+    const [left, setLeft] = useState(0);
+
+    const activeTab = (tab, index) => {
+        setLeft(index * 100)
+        setTimeout(() => {
+            setSelectedTab(index);
+        }, 500);
+        onTabChange(tab, index);
+    };
+
+  return (
+    <div className='switchingTabs'>
+          <div className="tabItems">
+              {data.map((tab, index) => (
+                  <span key={index} className={`tabItem ${selectedTab === index ? "active" : ""}`} onClick={()=>activeTab(tab,index)}>
+                      {tab}
+                  </span> 
+              ))}
+             <span className="movingBg" style={{left}}></span>
+      </div>
+    </div>
+  )
+}
+
+export default SwitchTab;

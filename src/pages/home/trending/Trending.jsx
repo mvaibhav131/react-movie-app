99b416fc5338480dd4688c162f9278de0d7a157d
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-04 14:16:59 +0530

    :100: create treading comp

---
+++ src/pages/home/trending/Trending.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+
+const Trending = () => {
+  return (
+    <div className='carouselSection'>
+          <ContentWrapper>
+              <span className="carouselTitle">
+                   
+              </span>
+      </ContentWrapper>
+    </div>
+  )
+}
+
+export default Trending;

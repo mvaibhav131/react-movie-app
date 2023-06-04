9694006d38d9d7e26de0da1a4efb01696d38141d
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-04 14:17:24 +0530

    :+1: Trending route added to home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
-// import React from 'react';
+import React from 'react';
 import "./style.scss";
 import HeroBanner from './heroBanner/HeroBanner';
+import Trending from './trending/Trending';
 
 const Home = () => {
   return (
     <div className='homePage'>
-      <HeroBanner/>
+      <HeroBanner />
+      <Trending/>
     </div>
   )
 }

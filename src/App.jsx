510c32033f8056261888cd2dec661e939062fad9
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-05 14:38:05 +0530

    :radio_button: adding login and register routes

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,9 @@ import Explore from './pages/explore/Explore';
 import {PageNotFound} from './pages/404/PageNotFound';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import Register from './components/auth/registration/Registration';
+import Login from './components/auth/login/Login';
+
 
 function App() {
   const dispatch=useDispatch();
@@ -60,6 +63,8 @@ useEffect(()=>{
     <Route path="/:mediaType/:id" element={<Details/>}  />
     <Route path="/search/:query" element={<SearchResult/>}  />
     <Route path="/explore/:mediaType" element={<Explore/>}  />
+    <Route path='/register' element={<Register />} />
+    <Route path="/login" element={<Login/>} />
     <Route path="*" element={<PageNotFound/>}  />
   </Routes>
   <Footer/>

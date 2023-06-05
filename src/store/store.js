614343a42a18601460fb148fea4fae695769903c
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-05 14:40:22 +0530

    :star: new user authSlice added to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import homeSlice from "./homeSlice";
+import authSlice from "./authSlice";
 
 
 export const store= configureStore({
     reducer:{
-        home:homeSlice, 
+        home: homeSlice, 
+        user:authSlice,
     },
 })

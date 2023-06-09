641ad7ab27c5bdd1b4fcf15fa0a2e025417a003f
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-09 13:38:24 +0530

    :sparkles: remove extra space

--- src/components/switchTabs/SwitchTab.jsx
+++ src/components/switchTabs/SwitchTab.jsx
@@ -2,29 +2,29 @@ import React, { useState } from 'react';
 import "./style.scss";
 
 const SwitchTab = ({ data, onTabChange }) => {
-    const [selectedTab, setSelectedTab] = useState(0);
-    const [left, setLeft] = useState(0);
+  const [selectedTab, setSelectedTab] = useState(0);
+  const [left, setLeft] = useState(0);
 
-    const activeTab = (tab, index) => {
-        setLeft(index * 100)
-        setTimeout(() => {
-            setSelectedTab(index);
-        }, 500);
-        onTabChange(tab, index);
-    };
+  const activeTab = (tab, index) => {
+    setLeft(index * 100)
+    setTimeout(() => {
+      setSelectedTab(index);
+    }, 500);
+    onTabChange(tab, index);
+  };
 
   return (
     <div className='switchingTabs'>
-          <div className="tabItems">
-              {data.map((tab, index) => (
-                  <span key={index} className={`tabItem ${selectedTab === index ? "active" : ""}`} onClick={()=>activeTab(tab,index)}>
-                      {tab}
-                  </span> 
-              ))}
-             <span className="movingBg" style={{left}}></span>
+      <div className="tabItems">
+        {data.map((tab, index) => (
+          <span key={index} className={`tabItem ${selectedTab === index ? "active" : ""}`} onClick={() => activeTab(tab, index)}>
+            {tab}
+          </span>
+        ))}
+        <span className="movingBg" style={{ left }}></span>
       </div>
     </div>
   )
-}
+};
 
 export default SwitchTab;

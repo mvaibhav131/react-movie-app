1c6aa607981daab7b1406ad542ac8b24c57ade9f
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-09 13:51:17 +0530

    :bug: fixing createSlice issue

--- src/store/homeSlice.js
+++ src/store/homeSlice.js
@@ -1,20 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const homeSlice=createSlice({
-    name:"home",
-    initialState:{
-        url:{},
-        genres:{}
+export const homeSlice = createSlice({
+    name: "home",
+    initialState: {
+        url: {},
+        genres: {}
     },
-    reducers:{
-        getApiConfiguration:(state,action)=>{
-            state.url=action.payload;
+    reducers: {
+        getApiConfiguration: (state, action) => {
+            state.url = action.payload;
         },
-        getGenres:(state,action)=>{
-            state.genres=action.payload
+        getGenres: (state, action) => {
+            state.genres = action.payload
         }
     },
-})
+});
 
 export const {getApiConfiguration,getGenres}=homeSlice.actions;
 export default homeSlice.reducer;

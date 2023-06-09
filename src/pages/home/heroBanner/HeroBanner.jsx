31700dd2ca4254fd3a6b8f88a1960ee3646cdf64
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-09 13:44:57 +0530

    :sparkles: remove extra space

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -7,44 +7,43 @@ import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
 import Img from '../../../components/lazyLoadImage/Img';
 
 const HeroBanner = () => {
-    const [background,setBackground]=useState("");
-    const [query,setQuery]=useState("");
-    const navigate=useNavigate();
-    const {url}=useSelector((state)=>state.home)
+    const [background, setBackground] = useState("");
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+    const { url } = useSelector((state) => state.home)
 
-    const {data,loading} = useFetch("/movie/upcoming");
+    const { data, loading } = useFetch("/movie/upcoming");
 
-    useEffect(()=>{
-       const bg= url.backdrop+data?.results?.[Math.floor(Math.random() * 20)].backdrop_path
-        setBackground(bg)                                                
-    },[data]);
+    useEffect(() => {
+        const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)].backdrop_path
+        setBackground(bg)
+    }, [data]);
 
-    const searchQueryHandler=(event)=>{
-        if(event.key === "Enter" && query.length>0){
-             navigate(`/search/${query}`)
+    const searchQueryHandler = (event) => {
+        if (event.key === "Enter" && query.length > 0) {
+            navigate(`/search/${query}`)
         }
     };
-  return (
- <div className="heroBanner">
-   {!loading && <div className="backdrop-img">
-       <Img src={background}/>
-    </div>}
-    <div className="opacity-layer">
-        
-    </div>
-    <ContentWrapper>
-        <div className="heroBannerContent">
-            <span className="title">Welcome...</span>
-            <span className="subTitle">Your Favourites Movies, TV Shows and Doscover...</span>
-            <div className="searchInput">
-                <input type="text" placeholder='Search movie or tv shows...' onChange={(e)=>setQuery(e.target.value)} onKeyUp={searchQueryHandler} />
-                <button onClick={()=>navigate(`/search/${query}`)}>Search</button>
+    return (
+        <div className="heroBanner">
+            {!loading && <div className="backdrop-img">
+                <Img src={background} />
+            </div>}
+            <div className="opacity-layer">
             </div>
-        </div>
+            <ContentWrapper>
+                <div className="heroBannerContent">
+                    <span className="title">Welcome...</span>
+                    <span className="subTitle">Your Favourites Movies, TV Shows and Doscover...</span>
+                    <div className="searchInput">
+                        <input type="text" placeholder='Search movie or tv shows...' onChange={(e) => setQuery(e.target.value)} onKeyUp={searchQueryHandler} />
+                        <button onClick={() => navigate(`/search/${query}`)}>Search</button>
+                    </div>
+                </div>
     
-    </ContentWrapper>
- </div>
-  )
-}
+            </ContentWrapper>
+        </div>
+    )
+};
 
 export default HeroBanner

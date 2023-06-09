8d59e6a60aaf2c02402b29e9728ebfd8cde27e7b
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-09 13:40:57 +0530

    :sparkles: remove extra space

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -11,8 +11,7 @@ import Recommendation from './carousels/Recommendation';
 const Details = () => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
-  const { data:credits, loading:creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
-
+  const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
 
   return (
     <div>
@@ -20,9 +19,9 @@ const Details = () => {
       <Cast data={credits?.cast} loading={creditsLoading} />
       <VideosSection data={data} loading={loading} />
       <Similar mediaType={mediaType} id={id} />
-      <Recommendation mediaType={mediaType} id={id}/>
-  </div>
-    )
-}
+      <Recommendation mediaType={mediaType} id={id} />
+    </div>
+  )
+};
 
 export default Details;

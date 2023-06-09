f4cde8eb46293533d22a77e76fe5d4f49970ecdb
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-09 13:34:29 +0530

    :sparkles: remove extra space

--- src/components/circleRating/CircleRating.jsx
+++ src/components/circleRating/CircleRating.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-
 import "./style.scss";
 
 const CircleRating = ({ rating }) => {

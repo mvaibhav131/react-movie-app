9cca6b64e0505b1ada1029cea78978a4e4863d34
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-09 13:37:15 +0530

    :bug: fix

--- src/components/lazyLoadImage/img.jsx
+++ src/components/lazyLoadImage/img.jsx
@@ -1,4 +1,4 @@
-// import React from "react";/
+import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 

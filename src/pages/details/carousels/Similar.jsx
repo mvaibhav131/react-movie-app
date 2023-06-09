631be0dfa064f3113b3198e1dcf91831d1f43257
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-09 13:40:08 +0530

    :sparkles: remove extra space

--- src/pages/details/carousels/Similar.jsx
+++ src/pages/details/carousels/Similar.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import Carousel from "../../../components/carousel/Carousel";
 import useFetch from "../../../hooks/useFetch";
 
-
 const Similar = ({ mediaType, id }) => {
     const { data, loading, error } = useFetch(`/${mediaType}/${id}/similar`);
-
     const title = mediaType === "tv" ? "Similar TV Shows" : "Similar Movies";
 
     return (
